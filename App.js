a33cdf80dcb5199a3660f76d7451e6d6622938a0
olakunle <oraclee10gee@gmail.com>
2019-02-12 13:41:05 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+      const students = [
+        {
+            name: 'chudi',
+            age: 10,
+            hobbies: ['singing', 'dancing']
+        },
+        {
+            name: 'tony',
+            age: 20,
+            hobbies: "eating",
+        },
+        {
+            name: 'arnold',
+            age: 25,
+            hobbies: "playing",
+        },
+        {
+            name: 'chudi',
+            age: 28,
+            hobbies: "running",
+        },
+        {
+            name: 'richard',
+            age: 20,
+            hobbies: 'coding'
+        }
+          
+    ]
+    
+    console.log(students);
+
+    );
+  }
+}
+
+export default App;
